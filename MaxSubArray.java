a4528eee273ab883182c2c62d288ee43a3d91158
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2023-01-03 00:39:55 +0530

    bruteforce solution for find max subArray

--- MaxSubArray.java
+++ MaxSubArray.java
@@ -12,6 +12,8 @@ package Array.ARRAY;
 
 public class MaxSubArray {
 	
+	
+//	Approach - 1 (Kadans Algorithm)
     public static int maxSubArray(int[] nums) {
         int curr_sum = nums[0];
         int best_sum = nums[0];
@@ -29,10 +31,28 @@ public class MaxSubArray {
         return best_sum;
         
     }
+    
+//    Approach -2 (Brute Force)
+    public static int maxSubArray2(int[] arr) {
+        int max_sum = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            int curr_sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                curr_sum += arr[j];
+                if (curr_sum > max_sum) {
+                    max_sum = curr_sum;
+                }
+            }
+        }
+        return max_sum;
+    }
 
 	public static void main(String[] args) {
 	 int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
-	 int msa = maxSubArray(arr);
+//	 int msa = maxSubArray(arr);
+//	 System.out.println(msa);
+	 
+	 int msa = maxSubArray2(arr);
 	 System.out.println(msa);
 	}
 
