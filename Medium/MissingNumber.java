89d2bcfb0502ea9bf1297b02d580b77b18ede4bf
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2023-01-04 21:09:07 +0530

    Improving So;ution

--- Medium/MissingNumber.java
+++ Medium/MissingNumber.java
@@ -1,51 +1,59 @@
 package Array.ARRAY.Medium;
 
-//Question Statement
-//Given an array of size N-1 such that it only contains distinct
-//integers in the range of 1 to N. Find the missing element.
-//Input:
-//N = 5
-//A[] = {1,2,3,5}
-//Output: 4
-
-
 import java.util.Arrays;
 
 public class MissingNumber {
 	
-//PPROACHJ - 1
+//	QUESTION statement
 	
-	public static int MissingNumberInArr(int array[], int n) {
-      // Your Code Here
-      int missing =n;
-      Arrays.sort(array);
-      for(int i =0; i < array.length;i++){
-          
-          if(array[i] != (i+1)){
-              missing  = i+1;
-              break;
-          }
-      }
-      return missing;
-      
-  }
+//	Given an array nums containing n distinct numbers in the range [0, n],
+//	        return the only number in the range that is missing from the array.
+//			Input: nums = [3,0,1]
+//			Output: 2
+//			Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 
+//			2 is the missing number in the range since it does not appear in nums.
 
+//			Input: nums = [0,1]
+//			Output: 2
+//			Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2].
+//	        2 is the missing number in the range since it does not appear in nums.
 	
-//	APPROACH -2 
 	
-	public static int MissingNumberInArr2(int array[], int n) {
-		int actualSum = n*(n+1)/2;
-		int sum = 0;
-		for(int i=0; i<n-1; i++) {
-			sum+=array[i];
-		}
-		return actualSum - sum;
-	}
+//	Approach - 1 
 	
+	 public static int missingNumber(int[] nums) {
+	        int n=nums.length;
+	        int actualSum = n*(n+1)/2;
+			int sum = 0;
+			for(int i=0; i<n; i++) {
+				sum+=nums[i];
+			}
+			return actualSum - sum;
+	        
+	    }
+	
+	 
+//	 Aprroach - 2 
+	 public static int missingNumberBetter(int nums[]) {
+	         int missing = nums.length;
+	         Arrays.sort(nums);
+	         for(int i=0; i<nums.length; i++){
+	             if(nums[i] != i){
+	                 missing = i;
+	                 break;
+	             }
+	         }
+	         return missing;
+	        
+	 }
+	 
+	 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int A[] = {1,3,4,5,6};
-		int ans = MissingNumberInArr(A , 5);
+//		int A[] = {1,3,4,5,0};
+		int A[] = {1,3,4,2,0};
+
+		int ans = missingNumberBetter(A);
 		System.out.println(ans);
 
 	}
